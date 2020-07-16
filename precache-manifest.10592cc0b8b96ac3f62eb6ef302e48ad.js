41c077e9bbddcf8112885f5de8b90f53a10b997c
goutam patnaik <goutam.nitr@gmail.com>
2020-07-17 02:28:06 +0530

    Updates

---
+++ precache-manifest.10592cc0b8b96ac3f62eb6ef302e48ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff52c6b545b3c437bca94ab0071c858",
+    "url": "/react-redux-api-template/index.html"
+  },
+  {
+    "revision": "d38318167324c756c258",
+    "url": "/react-redux-api-template/static/css/main.17d3647c.chunk.css"
+  },
+  {
+    "revision": "f5a44b892dcabc4eaf9b",
+    "url": "/react-redux-api-template/static/js/2.08127b82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-api-template/static/js/2.08127b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38318167324c756c258",
+    "url": "/react-redux-api-template/static/js/main.644bf423.chunk.js"
+  },
+  {
+    "revision": "ef6c982388742aec95c2",
+    "url": "/react-redux-api-template/static/js/runtime-main.1001a5cd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-redux-api-template/precache-manifest.28efe39871c9e73e83981476bf5496a6.js"
+  "/react-redux-api-template/precache-manifest.10592cc0b8b96ac3f62eb6ef302e48ad.js"
 );
 
 self.addEventListener('message', (event) => {
